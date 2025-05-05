b7e6883955e56c0241f3f291bb10d0d7c068f7c8
Bomb_Sh0t <bombshotyt@gmail.com>
2025-05-04 21:10:24 -0400

    Initial Commit

---
+++ src/main/java/net/bomb/additions/BombsAdditionsClient.java
@@ -0,0 +1,10 @@
+package net.bomb.additions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BombsAdditionsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
